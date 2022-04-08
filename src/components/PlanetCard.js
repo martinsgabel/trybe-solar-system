1eb8418d376d4ee57e22d62d2857db5a461e8f4b
martins <gabrielamspinola@gmail.com>
2022-04-08 16:06:06 -0300

    Requisitos 1 à 9, sem revisar

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Title from './Title';
+import PlanetCards from './PlanetCard';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {planets
+          .map((planet, ind) => (<PlanetCards
+            key={ ind }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
